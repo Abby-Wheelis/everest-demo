4404a4a1212a02013786e89f26566dae9cdf9887
Abby Wheelis <Abigail.Wheelis@nrel.gov>
2025-07-31 13:48:18 -0600

    copy citrines approach to dockerfile

--- citrineos/apply-compile-patches.sh
+++ citrineos/apply-compile-patches.sh
@@ -6,4 +6,4 @@ cp -r Server/data . #where does it need to end up?
 cp -r Server/hasura-metadata . #where does it need to end up?
 
 #this needs to happen before compiling
-cp ../everest-demo/citrineos/Dockerfile Server/Dockerfile
+# cp ../everest-demo/citrineos/Dockerfile Server/Dockerfile

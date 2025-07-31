dc81a123d5e8d2e1abdc65fbe9e76912887af006
Abby Wheelis <Abigail.Wheelis@nrel.gov>
2025-07-31 13:14:27 -0600

    install the thing?

--- citrineos/apply-compile-patches.sh
+++ citrineos/apply-compile-patches.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-# This is a patch to the source code, so we need to apply it
-# before we build.
-# And there is no harm in turning off OCSP completely
-
+#this actually needs to happen after compiling
 echo "Copying data into the container!"
 cp -r Server/data . #where does it need to end up?
 cp -r Server/hasura-metadata . #where does it need to end up?
 
-ls ../everest-demo/citrineos
-# ls ../citrineos
-# pwd
+#this needs to happen before compiling
 cp ../everest-demo/citrineos/Dockerfile Server/Dockerfile

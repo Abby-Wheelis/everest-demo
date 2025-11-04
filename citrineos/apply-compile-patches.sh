052e0ee8801aad0d8bd8edb52232e9b6b8ec0644
Abby Wheelis <Abigail.Wheelis@nrel.gov>
2025-11-03 18:56:03 -0700

    apply compile patches again?

--- citrineos/apply-compile-patches.sh
+++ citrineos/apply-compile-patches.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-#this actually needs to happen after compiling
+# #this actually needs to happen after compiling
 echo "Copying data into the container!"
-cp -r Server/data . #where does it need to end up?
-cp -r Server/hasura-metadata . #where does it need to end up?
+# cp -r Server/data . #where does it need to end up?
+# cp -r Server/hasura-metadata . #where does it need to end up?
 
 #this needs to happen before compiling
 # cp ../everest-demo/citrineos/Dockerfile Server/Dockerfile
+
+git clone https://github.com/citrineos/citrineos-core.git to-copy-templates
+cp -r to-copy-templates/Server/data .
+cp -r to-copy-templates/Server/hasura-metadata .
+# rm -rf to-copy-templates
